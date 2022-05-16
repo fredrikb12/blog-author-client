da884b70c69c4435064b5016cb9c7f1f49619877
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-16 15:58:00 +0200

    Create redirection system on home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
 function HomeRoute() {
-  const [user] = useOutletContext();
+  const [user, setUser] = useOutletContext();
+  const [posts, setPosts] = useState([]);
   const nav = useNavigate();
+
   useEffect(() => {
-    if (user) nav("/post");
+    if (user) nav("/posts");
     else nav("/sign-in");
   }, [user, nav]);
+
+  /*useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:3000/auth/posts", {
+        credentials: "include",
+        method: "GET",
+        mode: "cors",
+      });
+      const data = response.json();
+      console.log(data);
+    }
+    fetchPosts();
+  }, [user, nav]);*/
 }
 
 export default HomeRoute;
