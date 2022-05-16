2df597ac5b95ccc84a3b9b11d82a230cb91506c6
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-16 16:38:39 +0200

    Display date and author of each post

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 function App() {
   const [user, setUser] = useState(null);
   return (
-    <div>
+    <>
       <Outlet context={[user, setUser]} />
-    </div>
+    </>
   );
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,33 +1,39 @@
 import { useEffect, useState } from "react";
+import { fetchPosts } from "../helpers/fetchPosts";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function fetchPosts() {
-      const response = await fetch("http://localhost:3000/auth/posts", {
-        credentials: "include",
-        method: "GET",
-        mode: "cors",
-      });
-      const data = await response.json();
-      console.log(data);
+    async function initialLoad() {
+      const data = await fetchPosts();
       setPosts(() => data.posts);
     }
-    fetchPosts();
+    initialLoad();
   }, []);
+
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
+
   return (
-    <div>
+    <main>
       <h1>Posts:</h1>
       {posts.map((post) => {
         return (
           <article key={post._id}>
             <h2>{post.title}</h2>
             <p>{post.text}</p>
+            <div style={{ display: "flex", gap: "12px" }}>
+              <p>
+                {post.author.first_name} {post.author.last_name}
+              </p>
+              <p>{new Date(post.updatedAt).toLocaleDateString()}</p>
+            </div>
           </article>
         );
       })}
-    </div>
+    </main>
   );
 }
 
