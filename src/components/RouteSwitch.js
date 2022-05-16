d98122702b6f27ffac52e7002333fc5642ba38d0
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-16 16:02:07 +0200

    Add successful login route

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import HomeRoute from "./Home";
+import LoginSuccess from "./LoginSuccess";
 import Posts from "./Posts";
 import SignIn from "./SignIn";
 
@@ -13,6 +14,7 @@ function RouteSwitch() {
           <Route path="/posts" element={<Posts />} />
           <Route path="/posts/:postId" element={<h1>Post</h1>} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/success" element={<LoginSuccess/>} />
           <Route path="*" element={<h1>There's nothing here. </h1>} />
         </Route>
       </Routes>
