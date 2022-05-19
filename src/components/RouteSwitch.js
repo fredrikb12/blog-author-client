035915834837ef7bfe14d2a195468d5aaaa79de6
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-19 20:21:54 +0200

    Add route for creating new posts

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import HomeRoute from "./HomeRoute";
 import LoginSuccess from "./LoginSuccess";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import Posts from "./Posts";
 import SignIn from "./SignIn";
@@ -13,6 +14,7 @@ function RouteSwitch() {
         <Route path="/" element={<App />}>
           <Route index element={<HomeRoute />} />
           <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/new" element={<NewPost />} />
           <Route path="/posts/:postId" element={<Post />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/success" element={<LoginSuccess />} />
