71fc000938357eba9d8c8f4567c80050a76e73f0
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-19 14:07:54 +0200

    Add global styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { GlobalStyle } from "../styled/GlobalStyle";
 
 function App() {
   const [user, setUser] = useState(null);
   return (
     <>
+      <GlobalStyle />
       <Outlet context={[user, setUser]} />
     </>
   );
