9a4f3c60fd0615e99999cec6aedc40fb3f0db209
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-23 20:16:56 +0200

    Add navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { GlobalStyle } from "../styled/GlobalStyle";
 
 function App() {
   const [user, setUser] = useState(null);
   return (
     <>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10vh",
+        }}
+      >
+        <nav>
+          <ul
+            style={{
+              display: "flex",
+              listStyle: "none",
+              gap: "250px",
+              fontSize: "1.5rem",
+            }}
+          >
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/posts/new">
+              <li>New</li>
+            </Link>
+          </ul>
+        </nav>
+      </header>
       <GlobalStyle />
       <Outlet context={[user, setUser]} />
     </>
