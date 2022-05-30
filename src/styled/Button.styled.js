707a1f0dda84ac93d69394684d3f79296d11a43f
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-30 15:15:00 +0200

    Implement generic styled button

---
+++ src/styled/Button.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  font-size: 1.2rem;
+  padding: 6px 16px;
+  border-radius: 12px;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
