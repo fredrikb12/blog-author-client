2f938878ae42ecea136ef541fe2b0000854749e0
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-30 17:16:20 +0200

    Fix postcard styling

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,30 +2,27 @@ import { Link } from "react-router-dom";
 import { LightButton, StyledButton } from "../styled/Button.styled";
 import { StyledPostCard } from "../styled/PostCard.styled";
 import Button from "./Button";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+`;
 
 function PostCard({ post, handleClick }) {
   return (
     <StyledPostCard>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
         <Link to={`/posts/${post._id}`}>
           <h2>{post.title}</h2>
         </Link>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <LightButton
-            onClick={() =>
-              handleClick("update", { ...post, published: !post.published })
-            }
-          >
-            {post.published ? "Unpublish" : "Publish"}
-          </LightButton>
-          <LightButton
-            className="delete-button"
-            onClick={() => handleClick("delete", post)}
-          >
-            Delete
-          </LightButton>
-          <p>{post.error ? post.error : ""}</p>
-        </div>
       </div>
       <p>{post.text}</p>
       <div style={{ display: "flex", gap: "12px" }}>
@@ -39,6 +36,22 @@ function PostCard({ post, handleClick }) {
             : null}
         </p>
       </div>
+      <ButtonContainer>
+        <LightButton
+          onClick={() =>
+            handleClick("update", { ...post, published: !post.published })
+          }
+        >
+          {post.published ? "Unpublish" : "Publish"}
+        </LightButton>
+        <LightButton
+          className="delete-button"
+          onClick={() => handleClick("delete", post)}
+        >
+          Delete
+        </LightButton>
+        <p>{post.error ? post.error : ""}</p>
+      </ButtonContainer>
     </StyledPostCard>
   );
 }
--- src/styled/PostCard.styled.js
+++ src/styled/PostCard.styled.js
@@ -2,4 +2,9 @@ import styled from "styled-components";
 
 export const StyledPostCard = styled.article`
   padding: 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.darkShade};
+
+  &:last-child {
+    border-bottom: none;
+  }
 `;
